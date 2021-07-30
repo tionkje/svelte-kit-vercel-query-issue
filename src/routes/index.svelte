8e8e6e00b025e21ad17cc04a44783a88fe0b9ddc
Bastiaan Tiobook <deknudtb@gmail.com>
2021-07-30 23:21:26 +0200

    log and print query

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,16 @@
+<script context="module">
+  import { writable } from "svelte/store";
+
+  export async function load({ page }) {
+    const { query } = page;
+    const name = query.get('name');
+    console.log("load", name)
+    return { props: { name } };
+  }
+</script>
+  <script>
+    export let name;
+  </script>
 <h1>Welcome to SvelteKit</h1>
+{name}
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
